5dc064cadfa5d62ce46a6dc6194f1be98c389d58
Jashan_workspace <Jashan@aws_workspace>
2023-02-19 14:42:30 -0800

    Exercise 3 and output file

--- algorithms/algorithms.cc
+++ algorithms/algorithms.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <vector> 
+#include <numeric>
 ////////////////////////////////////
 // INCLUDE NECESSARY HEADERS HERE //
 ////////////////////////////////////
@@ -12,56 +14,52 @@ void print_vector (const vector<int>& v);
 
 void test_algorithms (int N)
 {
+  vector<int> v ( N, 0);
+  print_vector(v);
   //////////////////////////////
   // CREATE A VECTOR v OF int //
   // WITH SIZE N              //
   // FILLED WITH ZEROS        //
   //////////////////////////////
 
-
+  iota(v.begin(), v.end(), 5);
   print_vector(v);
-
   ///////////////////////////////
   // USE iota TO FILL v        //
   // WITH VALUES STARTING AT 5 //
   ///////////////////////////////
 
-
-  print_vector(v);
-
+  reverse(v.begin()+2, v.begin()+5);
+  print_vector(v);  
   ///////////////////////////////////////
   // USE reverse TO REVERSE THE VALUES //
   // BETWEEN THE THIRD AND FIFTH FROM  //
   // LAST POSITIONS                    //
   ///////////////////////////////////////
 
-
+  
+  fill(v.begin(), v.begin() + 5, 20);
   print_vector(v);
-
   /////////////////////////////////////
   // USE fill TO FILL THE FIRST FOUR //
   // VALUES WITH 20s                 //
   /////////////////////////////////////
 
-
+  sort(v.begin() +1, v.end() -1);
   print_vector(v);
-
   ////////////////////////////////////////
   // USE sort TO SORT ALL BUT THE FIRST //
   // AND LAST VALUES                    //
   ////////////////////////////////////////
 
-
+  
+  sort(v.begin(), v.end(), [](int a, int b){return ((a%2) < (b%2)); }); 
   print_vector(v);
-
   ///////////////////////////////////////////
   // USE sort WITH A COMPARISON LAMBDA     //
   // TO SORT ALL EVEN NUMBERS TO THE       //
   // FRONT AND ALL ODD NUMBERS TO THE BACK //
   ///////////////////////////////////////////
-
-
-  print_vector(v);
 }
 
 int main ()
